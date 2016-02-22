e7b8bdd9c3153e7156e98142748723d5bb96a58a
seaders <seaders69@gmail.com>
2016-02-22 18:33:56 +0000

    A few prefabs, and partnering C# scripts for MessageCenter logic.

---
+++ unity3d/Assets/Swrve/UnitySwrveDemo/CSharp/ModalQuestionComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ModalQuestionComponent : MonoBehaviour {
+    public Text titleText;
+    public Text questionText;
+    public Button positiveButton;
+    public Button negativeButton;
+}
