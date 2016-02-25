d147f912e9bf5126bada9b9fbc7a49ac931dedd0
seaders <seaders69@gmail.com>
2016-02-25 17:31:31 +0000

    Wrappers for UNITY_4_7 flags.

--- unity3d/Assets/Editor/SwrveCommonBuildComponent.cs
+++ unity3d/Assets/Editor/SwrveCommonBuildComponent.cs
@@ -16,13 +16,13 @@ public class SwrveCommonBuildComponent
 
     protected static void BuildIOS (string fileName, BuildOptions opt, string[] mainScenes, string bundleIdentifier)
     {
-#if (UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6)
+#if (UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7)
         EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.iPhone);
 #else
         EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.iOS);
 #endif
         PlayerSettings.bundleIdentifier = bundleIdentifier;
-#if (UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6)
+#if (UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7)
         string error = BuildPipeline.BuildPlayer (mainScenes, fileName, BuildTarget.iPhone, opt);
 #else
         string error = BuildPipeline.BuildPlayer (mainScenes, fileName, BuildTarget.iOS, opt);
