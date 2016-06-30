ebd6653620d784c67fe13486421dca742c51ae6e
seaders <seaders@gmail.com>
2016-06-30 18:05:40 +0100

    Bring in new logic for showing conversation from main SDK (needs refactoring / moved to a common area).

--- native/ios/SwrveUnitySDK/UnitySDK/UnitySwrveCommonMessageController.h
+++ native/ios/SwrveUnitySDK/UnitySDK/UnitySwrveCommonMessageController.h
@@ -1,9 +1,13 @@
 #import "SwrveMessageEventHandler.h"
 #import "SwrveBaseConversation.h"
 
+@class SwrveConversationItemViewController;
+
 @interface UnitySwrveMessageEventHandler : NSObject<SwrveMessageEventHandler>
 
 -(SwrveBaseConversation*) conversationFromString:(NSString*)conversation;
 -(void) showConversationFromString:(NSString*)conversation;
+
+@property (nonatomic, retain) SwrveConversationItemViewController* swrveConversationItemViewController;
 
 @end
