ba879339f300f7f2a6863d8c932f7340e1563a9f
Sergio-Mira <sergio.mira@swrve.com>
2016-08-30 12:10:03 +0100

    Started 4.7

--- unity3d/Assets/Swrve/SwrveSDK/SwrveSDK.cs
+++ unity3d/Assets/Swrve/SwrveSDK/SwrveSDK.cs
@@ -35,7 +35,7 @@ using Swrve.IAP;
 /// </remarks>
 public partial class SwrveSDK : ISwrveAssetController
 {
-    public const string SdkVersion = "4.6";
+    public const string SdkVersion = "4.7";
 
     private int gameId;
     /// <summary>
