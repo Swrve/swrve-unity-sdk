163d5aaae6d386a65e370d39d49e81914091afc3
seaders <seaders@gmail.com>
2016-09-12 19:40:29 +0100

    Add UWP post process for UWP.

---
+++ unity3d/Assets/Swrve/Editor/SwrveUWPPostProcess.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_WSA
+
+using UnityEngine;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class SwrveUWPPostProcess : SwrveCommonBuildComponent
+{
+    [PostProcessBuild(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        SwrveLog.Log ("SwrveQAPostProcess (UWP)");
+        DirectoryCopy("extras", Path.Combine(pathToBuiltProject, "extras"), false, true);
+        CopyFile(Path.Combine(pathToBuiltProject, "UnityCommon.props"), "UWP/Assembly-CSharp/UnityCommon.props");
+        CopyFile(Path.Combine(pathToBuiltProject, "UnityCommon.props"), "UWP/Assembly-CSharp-firstpass/UnityCommon.props");
+    }
+}
+#endif
